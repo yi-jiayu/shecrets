96b726a41c24ac3ab0be01f3d28e602d701a80e8
Jiayu Yi <yi-jiayu@users.noreply.github.com>
2019-02-24 11:21:27 +0800

    Run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ extern crate toml;
 
 use std::fmt::Display;
 use std::io::{self, Read};
-use toml::Value as Value;
-use toml::value::Table as Table;
+use toml::value::Table;
+use toml::Value;
 
 struct Var<'a> {
     key: Vec<&'a str>,
@@ -19,40 +19,56 @@ fn walk(config: &Table) -> Vec<Var> {
             let mut prefix = prefix.to_owned();
             prefix.push(&k);
             match v {
-                Value::String(s) => vars.push(Var { key: prefix, value: Box::new(s.as_str()) }),
-                Value::Integer(i) => vars.push(Var { key: prefix, value: Box::new(i) }),
-                Value::Float(f) => vars.push(Var { key: prefix, value: Box::new(f) }),
-                Value::Boolean(b) => vars.push(Var { key: prefix, value: Box::new(b) }),
+                Value::String(s) => vars.push(Var {
+                    key: prefix,
+                    value: Box::new(s.as_str()),
+                }),
+                Value::Integer(i) => vars.push(Var {
+                    key: prefix,
+                    value: Box::new(i),
+                }),
+                Value::Float(f) => vars.push(Var {
+                    key: prefix,
+                    value: Box::new(f),
+                }),
+                Value::Boolean(b) => vars.push(Var {
+                    key: prefix,
+                    value: Box::new(b),
+                }),
                 Value::Table(t) => stack.push((prefix, t)),
-                _ => ()
+                _ => (),
             };
-        };
-    };
+        }
+    }
     vars
 }
 
 fn format_posix(var: &Var) -> String {
-    format!("{0}={1}; export {0}",
-            var.key.join("_").to_uppercase(),
-            var.value)
+    format!(
+        "{0}={1}; export {0}",
+        var.key.join("_").to_uppercase(),
+        var.value
+    )
 }
 
 fn format_vars(vars: &Vec<Var>) -> Vec<String> {
-    vars.iter()
-        .map(|var| format_posix(var))
-        .collect()
+    vars.iter().map(|var| format_posix(var)).collect()
 }
 
 fn main() -> Result<(), String> {
     let mut buffer = String::new();
-    io::stdin().read_to_string(&mut buffer).map_err(|e| format!("error reading standard input: {}", e))?;
-    let val = buffer.parse::<Value>().map_err(|e| format!("invalid toml: {}", e))?;
+    io::stdin()
+        .read_to_string(&mut buffer)
+        .map_err(|e| format!("error reading standard input: {}", e))?;
+    let val = buffer
+        .parse::<Value>()
+        .map_err(|e| format!("invalid toml: {}", e))?;
     // valid toml should always be a table at the top level
     let vars = walk(val.as_table().unwrap());
     let formatted = format_vars(&vars);
     for s in formatted {
         println!("{}", s);
-    };
+    }
     Ok(())
 }
 
@@ -82,17 +98,21 @@ nestedvar = "value2"
 doublynestedvar = "value3"
 [section2]
 nestedvar = "value4"
-"#.parse::<Value>().unwrap();
+"#
+        .parse::<Value>()
+        .unwrap();
         let vars = walk(config.as_table().unwrap());
         let formatted = format_vars(&vars);
-        assert_eq!(formatted,
-                   [
-                       "SOMEBOOLVAL=true; export SOMEBOOLVAL",
-                       "SOMEFLOATVAR=1.2; export SOMEFLOATVAR",
-                       "SOMEINTVAR=1; export SOMEINTVAR",
-                       "SECTION2_NESTEDVAR=value4; export SECTION2_NESTEDVAR",
-                       "SECTION_NESTEDVAR=value2; export SECTION_NESTEDVAR",
-                       "SECTION_NESTED_DOUBLYNESTEDVAR=value3; export SECTION_NESTED_DOUBLYNESTEDVAR",
-                   ]);
+        assert_eq!(
+            formatted,
+            [
+                "SOMEBOOLVAL=true; export SOMEBOOLVAL",
+                "SOMEFLOATVAR=1.2; export SOMEFLOATVAR",
+                "SOMEINTVAR=1; export SOMEINTVAR",
+                "SECTION2_NESTEDVAR=value4; export SECTION2_NESTEDVAR",
+                "SECTION_NESTEDVAR=value2; export SECTION_NESTEDVAR",
+                "SECTION_NESTED_DOUBLYNESTEDVAR=value3; export SECTION_NESTED_DOUBLYNESTEDVAR",
+            ]
+        );
     }
 }
